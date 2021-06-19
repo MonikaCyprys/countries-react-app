cbef08fdd0508ac1685565d1ce0508aba24334b0
Monika Cyprys <monko.projects@gmail.com>
2021-06-19 07:58:54 +0200

    fix button mode

--- src/components/nav.js/nav.js
+++ src/components/nav.js/nav.js
@@ -2,15 +2,42 @@ import React from "react";
 import styles from "./nav.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+
 const moonIcon = <FontAwesomeIcon icon={faMoon} />;
+const sunIcon = <FontAwesomeIcon icon={faSun} />;
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lightMode: true };
+    this.handleSwitcher = this.handleSwitcher.bind(this);
+  }
   handleSwitcher() {
-    document.querySelector(`[data-theme="light"]`)
-      ? document.body.setAttribute("data-theme", "dark")
-      : document.body.setAttribute("data-theme", "light");
+    this.setState(() => ({
+      lightMode: !this.state.lightMode,
+    }));
+  }
+  componentDidMount() {
+    if (this.state.lightMode) {
+      document.body.setAttribute("data-theme", "light");
+    } else {
+      document.body.setAttribute("data-theme", "dark");
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.lightMode !== this.state.lightMode) {
+      this.componentDidMount(this.state.lightMode);
+    }
   }
   render() {
+    const { lightMode } = this.state;
+    let buttonContent;
+    if (lightMode) {
+      buttonContent = " Dark Mode";
+    } else {
+      buttonContent = " Light Mode";
+    }
     return (
       <nav className={styles.AppNav}>
         <h1>Where in the world?</h1>
@@ -18,7 +45,8 @@ class Navigation extends React.Component {
           onClick={this.handleSwitcher}
           className={styles.SwitchModeButton}
         >
-          {moonIcon} Dark Mode
+          {lightMode ? moonIcon : sunIcon}
+          {buttonContent}
         </button>
       </nav>
     );
